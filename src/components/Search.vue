1c5f3adbb1a4966cdc604588b69c0a490c47304f
RockDrigo78 <rodrigobazandanino@hotmail.com>
2020-05-05 17:32:46 +0200

    first commit

---
+++ src/components/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-container">
+    <input
+      type="text"
+      placeholder="Search for todos ..."
+      v-model="query"
+      v-on:input="$emit('query-change', query)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.search-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+input {
+  padding: 10px;
+  width: 100%;
+  outline: none;
+}
+</style>
